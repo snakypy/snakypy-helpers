fe141d7b19777295851714e5de1660fe5a94814f
William C. Canin <william.costa.canin@gmail.com>
2021-05-13 02:24:07 -0300

    :hammer: :books: Updating doctring and refactoring code.

--- snakypy/helpers/files/generic.py
+++ snakypy/helpers/files/generic.py
@@ -5,38 +5,6 @@ from contextlib import suppress
 from shutil import SameFileError, copyfile
 
 
-def read_file(file_path: str, split: bool = False) -> Union[str, List[str]]:
-    """Reads a text file.
-
-    >>> import snakypy
-    >>> file = '/tmp/my_file.txt'
-    >>> snakypy.helpers.files.read_file(file)
-
-    or using from
-
-    >>> from snakypy.helpers.files import read_file
-    >>> file = '/tmp/my_file.txt'
-    >>> read_file(file)
-
-    Arguments:
-        **file_path {str}** -- You must receive the full/absolute file path.
-                                Keyword Arguments:
-        **split {bool}** -- If this option is True, a list will be returned where
-                                the breaks will be made using line skips. (default: {False})
-
-    Returns:
-        **[str|list]** -- By default it returns a string. If the option
-                         split=True, a list of line breaks will be returned.
-    """
-    try:
-        with open(file_path) as f:
-            if split:
-                return f.read().split("\n")
-            return f.read()
-    except FileNotFoundError as err:
-        raise FileNotFoundError(f'>>> File "{file_path}" does not exist. {err}')
-
-
 def create_file(content: Any, file_path: str, force: bool = False) -> bool:
     """
     Create a text file.
@@ -76,36 +44,58 @@ def create_file(content: Any, file_path: str, force: bool = False) -> bool:
             raise Exception(f">>> There was an error creating the file: {err}")
 
 
+def read_file(file_path: str, split: bool = False) -> Union[str, List[str]]:
+    """
+    Reads a text file.
+
+    >>> from snakypy import helpers
+    >>> file = '/tmp/my_file.txt'
+    >>> helpers.files.create_file('My content file', file, force=True)
+    True
+    >>> helpers.files.read_file(file)
+    'My content file'
+
+    Args:
+        file_path (str): You must receive the full/absolute file path.
+
+        split (bool): If this option is True, a list will be returned where
+                                the breaks will be made using line skips. (default: {False})
+
+    Returns:
+        [str|list]: By default it returns a string. If the option split=True,
+                    a list of line breaks will be returned.
+    """
+    try:
+        with open(file_path) as f:
+            if split:
+                return f.read().split("\n")
+            return f.read()
+    except FileNotFoundError as err:
+        raise FileNotFoundError(f'>>> File "{file_path}" does not exist. {err}')
+
+
 def backup_file(
     origin: str, destiny: str, *, date: bool = False, extension: bool = True
 ) -> None:
     """
         Create backup of files with date and time.
 
-    >>> import snakypy
-    >>> snakypy.helpers.files.backup_file("/tmp/file.txt", "/tmp/file.txt")
-    >>> snakypy.helpers.files.backup_file("/tmp/file.txt", "/tmp/file.txt", extension=False)
-    >>> snakypy.helpers.files.backup_file("/tmp/file.txt", "/tmp/file2.txt")
-    >>> snakypy.helpers.files.backup_file("/tmp/file.txt", "/tmp/file2.txt", date=True)
-
-    or using from
-
-    >>> from snakypy.helpers.files import backup_file
-    >>> backup_file("/tmp/file.txt", "/tmp/file.txt")
-    >>> backup_file("/tmp/file.txt", "/tmp/file.txt", extension=False)
-    >>> backup_file("/tmp/file.txt", "/tmp/file2.txt")
-    >>> backup_file("/tmp/file.txt", "/tmp/file2.txt", date=True)
+        >>> from snakypy import helpers
+        >>> helpers.files.backup_file("/tmp/file.txt", "/tmp/file.txt")
+        >>> helpers.files.backup_file("/tmp/file.txt", "/tmp/file.txt", extension=False)
+        >>> helpers.files.backup_file("/tmp/file.txt", "/tmp/file2.txt")
+        >>> helpers.files.backup_file("/tmp/file.txt", "/tmp/file2.txt", date=True)
 
     Arguments:
-        **origin (str)**: Source location of the file.
+        origin (str): Source location of the file.
 
-        **destiny (str)**: Destination location of the file.
+        destiny (str): Destination location of the file.
 
-        **date (bool, optional)**: Adds date and time to the generated backup file name.
+        date (bool, optional): Adds date and time to the generated backup file name.
                                    If the source and destination have the same values by default, the
                                    date is incorporated in the backup file name.Defaults to False.
 
-        **extension (bool, optional)**: Adds the file extension in the name after the subtitle. Defaults to True.
+        extension (bool, optional): Adds the file extension in the name after the subtitle. Defaults to True.
     """
 
     ext = ""
