def178f78ee55a76ad206b9cad4fa4e0d26eeeab
William C. Canin <william.costa.canin@gmail.com>
2021-05-14 16:33:55 -0300

    :hammer: Removing optional parameters and changing to mandatory.

--- snakypy/helpers/console/utils.py
+++ snakypy/helpers/console/utils.py
@@ -1,18 +1,18 @@
-def check_fg_bg_sgr(FG, BG, SGR, *args) -> None:
+def check_fg_bg_sgr(FG, BG, SGR, foreground, background, sgr) -> None:
     """
     Checks if the attributes of the functions that the foreground
     and background parameters are in accordance with their respective class.
     """
 
-    if args[0] and args[0] not in FG.__dict__.values():
+    if foreground and foreground not in FG.__dict__.values():
         raise AttributeError(
             'Attribute invalid in parameter "foreground". Must receive from FG class.'
         )
-    if args[1] and args[1] not in BG.__dict__.values():
+    if background and background not in BG.__dict__.values():
         raise AttributeError(
             'Attribute invalid in parameter "background". Must receive from BG class.'
         )
-    if args[2] and args[2] not in SGR.__dict__.values():
+    if sgr and sgr not in SGR.__dict__.values():
         raise AttributeError(
             'Attribute invalid in parameter "sgr". Must receive from SGR class.'
         )
