7f0759889365437914f4e5ed7d8a5d72e594d09d
William C. Canin <william.costa.canin@gmail.com>
2021-05-28 14:44:27 -0300

    :hammer: :sparkles: Adding function eqversion. Keep the versions the same on pyproject.toml and __init__.py

--- snakypy/helpers/files/generic.py
+++ snakypy/helpers/files/generic.py
@@ -1,8 +1,10 @@
-from os.path import exists
-from typing import Union, List, Any
-from datetime import datetime
 from contextlib import suppress
+from datetime import datetime
+from os.path import exists
 from shutil import SameFileError, copyfile
+from typing import Any, List, Union
+
+from tomlkit import dumps, parse
 
 
 def create_file(content: Any, file_path: str, force: bool = False) -> bool:
@@ -108,4 +110,58 @@ def backup_file(
         copyfile(origin, destiny_format)
 
 
-__all__ = ["read_file", "create_file", "backup_file"]
+def eqversion(filepath, version, *, key=("tool", "poetry", "version")):
+    """
+        Function to update the version of the pyproject.toml file according to the version of __init__.py
+
+    In __init__.py do:
+
+    .. code-block:: python
+
+        from snakypy.helpers.files import eqversion
+        pyproject = join(dirname(abspath(__file__)), "..", "pyproject.toml")
+        eqversion(pyproject, __version__)
+
+        NOTE: In ".." it defines as much of the directory that wants to return.
+
+    Args:
+        filepath: File path "pyproject.toml".
+        version: Enter the version found in the "__init__.py" file of the package.
+        key: You must pass the key level to access the version on "pyproject.toml".
+             By default, this value is used for Poetry.
+    """
+    with suppress(FileNotFoundError):
+        parsed = parse(read_file(filepath))
+        change = False
+
+        if not len(key):
+            raise KeyError(
+                f'Error function "{eqversion.__name__}": Enter the access keys. '
+                f'Must be a tuple. E.g: key=("key1", "key2",...)'
+            )
+        elif len(key) == 1:
+            if parsed[key[0]] != version:
+                parsed[key[0]] = version
+                change = True
+        elif len(key) == 2:
+            if parsed[key[0]][key[1]] != version:
+                parsed[key[0]][key[1]] = version
+                change = True
+        elif len(key) == 3:
+            if parsed[key[0]][key[1]][key[2]] != version:
+                parsed[key[0]][key[1]][key[2]] = version
+                change = True
+        elif len(key) == 4:
+            if parsed[key[0]][key[1]][key[2]][key[3]] != version:
+                parsed[key[0]][key[1]][key[2]][key[3]] = version
+                change = True
+        else:
+            raise KeyError(
+                f'Error function "{eqversion.__name__}": The number of supported keys has overflowed. Must be < 4.'
+            )
+
+        if change:
+            create_file(dumps(parsed), filepath, force=True)
+
+
+__all__ = ["read_file", "create_file", "backup_file", "eqversion"]
