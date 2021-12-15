a2985ab00536431b28d7ccba67d4460a76dabdd4
William C. Canin <william.costa.canin@gmail.com>
2021-12-14 23:50:33 -0300

    :hammer: Formatting code imports.

--- snakypy/helpers/catches/__init__.py
+++ snakypy/helpers/catches/__init__.py
@@ -1,2 +1,9 @@
 from .finders import find_objects, is_tool, tools_requirements
-from .generic import extension, shell, whoami
+from .generic import (
+    extension,
+    is_blank_file,
+    is_palindrome,
+    shell,
+    turned_palindrome,
+    whoami,
+)
