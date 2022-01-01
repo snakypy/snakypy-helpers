991343356505708ac64605f00e960d1f53fd1de0
William C. Canin <william.costa.canin@gmail.com>
2022-01-01 13:22:49 -0300

    :hammer: Refactoring code

--- snakypy/helpers/catches/__init__.py
+++ snakypy/helpers/catches/__init__.py
@@ -1,9 +1,2 @@
 from .finders import find_objects, is_tool, tools_requirements
-from .generic import (
-    extension,
-    is_blank_file,
-    is_palindrome,
-    shell,
-    turned_palindrome,
-    whoami,
-)
+from .generic import extension
